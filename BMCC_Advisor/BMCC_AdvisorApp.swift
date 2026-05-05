665e096a86137f8f9fa22661bf1524525e8ccd7a
ImUness <oddeoo30@gmail.com>
2026-05-04 22:00:26 -0400

    Initial Commit

---
+++ BMCC_Advisor/BMCC_AdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMCC_AdvisorApp.swift
+//  BMCC_Advisor
+//
+//  Created by Youness El Akri on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct BMCC_AdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMCC_AdvisorUITests/BMCC_AdvisorUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BMCC_AdvisorUITestsLaunchTests.swift
+//  BMCC_AdvisorUITests
+//
+//  Created by Youness El Akri on 5/4/26.
+//
+
+import XCTest
+
+final class BMCC_AdvisorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
